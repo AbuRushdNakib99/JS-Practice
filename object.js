43aade015351b3577cd3cdaddea58bcade9d27f9
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2023-04-02 05:37:49 +0600

    Object Declaration

---
+++ object.js
@@ -0,0 +1,18 @@
+// Object Declaration
+var point={
+    x:10,
+    y:20
+}
+
+point.z=30
+
+console.log(point)
+
+var obj=Object();
+obj.a=40;
+
+console.log(obj)
+
+var obj2=new Object();
+obj2.b=70;
+console.log(obj2)
