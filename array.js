30e545905c40c66522a6c57c9be708e866f2b37c
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2023-04-01 23:41:16 +0600

    array traverse

--- array.js
+++ array.js
@@ -4,5 +4,18 @@ console.log(arr)
 
 // Declare an Array by constructor
 
-let arr2=Array(10,20,30,40);
+let arr2=Array(10,20,30,40,50);
 console.log(arr2);
+
+// Array Traversing
+let sum=0
+
+for(let i=0;i<arr2.length;i++){
+    console.log(arr[i])
+}
+
+for(let i=0;i<arr.length;i++){
+    sum+=arr[i];
+}
+console.log(sum);
+
