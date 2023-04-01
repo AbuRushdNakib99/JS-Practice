a79155d2dbbe6200e645f2356445d97d39c5a875
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2023-04-01 23:53:30 +0600

    update and remove of an array

--- array.js
+++ array.js
@@ -19,3 +19,30 @@ for(let i=0;i<arr.length;i++){
 }
 console.log(sum);
 
+// Add an element last of the array
+arr.push(300);
+console.log(arr);
+
+// Add an Element first of the array
+arr.unshift(900);
+console.log(arr);
+
+// Add an Element specific address of an array
+arr.splice(3,0,20);
+console.log(arr);
+
+// Remove last element of an array
+arr.pop()
+console.log(arr);
+
+// Remove first element of an array
+arr.shift();
+console.log(arr);
+
+// Remove specific element of an array
+arr.splice(2,1)
+console.log(arr);
+
+// Remove an element using splice method of an array
+arr.splice(3,1,500);
+console.log(arr);
