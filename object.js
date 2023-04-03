ccdad4dc60439110daec1667eef6a2862fd5cbc5
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2023-04-03 09:36:26 +0600

    Comparing two objects

--- object.js
+++ object.js
@@ -41,3 +41,23 @@ console.log(point2)
 
 delete point2.x;
 console.log(point2)
+
+// Compare Two Objects
+var obj1={
+    a:10,
+    b:20
+}
+var obj2={
+    a:10,
+    b:20
+}
+
+if(obj1.a===obj2.a&&obj1.b==obj2.b){ //As usual way
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
+console.log(obj1)
+console.log(JSON.stringify(obj1)===JSON.stringify(obj2)) //Magical Way {Maximum times we use it}
