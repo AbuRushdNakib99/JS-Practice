a5a368d9f83b865644bb923757556ce05e2ed396
Abu Rushd Nakib <74209308+AbuRushdNakib99@users.noreply.github.com>
2023-04-03 09:23:35 +0600

    Accessing Object

--- object.js
+++ object.js
@@ -1,18 +1,34 @@
 // Object Declaration
-var point={
+// var point={
+//     x:10,
+//     y:20
+// }
+
+// point.z=30
+
+// console.log(point)
+
+// var obj=Object();
+// obj.a=40;
+
+// console.log(obj)
+
+// var obj2=new Object();
+// obj2.b=70;
+// console.log(obj2)
+
+// Accessing Object
+
+var point2={ //Used Maximum time
     x:10,
-    y:20
+    y:20,
+    z:15
 }
 
-point.z=30
+console.log(point2.x)
+console.log(point2.y);
+console.log(point2.x+point2.z);
 
-console.log(point)
+console.log(point2['x']+point2['z']); //Array Notation {Use When We Don't Know that which one we should use}
 
-var obj=Object();
-obj.a=40;
 
-console.log(obj)
-
-var obj2=new Object();
-obj2.b=70;
-console.log(obj2)
